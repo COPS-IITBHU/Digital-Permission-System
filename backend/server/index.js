fed7d261fbc9672eb2b26230db860de81c60f930
aayush0325 <96649223+aayush0325@users.noreply.github.com>
2024-10-17 17:32:02 +0530

    feat: basic mail setup completed

--- backend/server/index.js
+++ backend/server/index.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const dotenv = require('dotenv');
+
+// Importing Routers
+const mailRouter = require('./routes/mail');
+
+// Loading the env variables
+dotenv.config();
 
 const app = express();
 const PORT = 5000;
+
+// Loading routers on the app
+app.use('/api/mail', mailRouter)
 
 // Middleware
 app.use(cors());
@@ -15,7 +25,7 @@ app.get('/', (req, res) => {
 });
 
 // MongoDB connection (add your MongoDB URL)
-mongoose.connect('your-mongo-db-url', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGOURL)
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.log(err));
 
